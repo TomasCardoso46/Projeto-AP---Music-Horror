0c1bdb470dca346f701fee4dbd4ddda538de8da7
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-10-05 21:30:11 +0100

    2 Prototypes for guitar playing

---
+++ Music Horror/Assets/Scripts/Player/Guitar/Chord.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8f262f7223fa419834bcf2fe5956aed3f240ec4e2440cc4a6052d08f74cd610
+size 2529
---
+++ Music Horror/Assets/Scripts/Player/Guitar/ChordMouse.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0b0ec02783e7a05c66e4651c8acf2952b24cfa17ba7b4cdbc376554f1288bfc
+size 2969
