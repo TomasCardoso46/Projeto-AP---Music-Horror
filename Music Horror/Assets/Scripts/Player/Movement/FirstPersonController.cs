89a6a0780d1a96174d1f59aaaeedf30b44a8d0c4
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-10-12 17:12:09 +0100

    Fix to hand collider

--- Music Horror/Assets/Scripts/Player/Movement/FirstPersonController.cs
+++ Music Horror/Assets/Scripts/Player/Movement/FirstPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:828127d3d1fb8bff8b2c68efa3af25fedf5a00851b630acc6567e6df72cbb2a0
-size 3650
+oid sha256:9dbd160687f78c0fd1db85d689d670d01a2275e41404009729f8d11dc8805c15
+size 5236
