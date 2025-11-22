6d80b640237cf5ecac4fd34981e98f5255aed039
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-11-22 20:13:13 +0000

    Added jumpscare

--- Music Horror/Assets/Scripts/Enemy/Guitarist/Updated/EnemyAttack.cs
+++ Music Horror/Assets/Scripts/Enemy/Guitarist/Updated/EnemyAttack.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cabc5da6d86aa8096f5266c73f581cf42c33235b5c873a8954e4e71c5c68368a
-size 1573
+oid sha256:c45460cbcfeafe872b38cceac4b04c632e2247985116aae11b2540a358cf94f1
+size 1959
---
+++ Music Horror/Assets/Scripts/Enemy/Jumpscares/Jumpscare.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eef167a46d86ed7729a456fde53449fbc2f57de58a0323c8b7d7f7ff39c90594
+size 2737
