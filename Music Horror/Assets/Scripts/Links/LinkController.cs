b6c03e9807f3ed22397234ceddf1d88692e2b15f
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-01-04 11:53:32 +0000

    Clean traversal through links, and setup for special door animation

---
+++ Music Horror/Assets/Scripts/Links/LinkController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6fc8dfde45a9dd04a7e050a21507b660804c9b4d4057d48058cc6e65d6176798
+size 4164
---
+++ Music Horror/Assets/Scripts/Links/LinkSettings.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:34795fd1b814eaaf4c1c29f43e679137c6c8655f3d0507312f4b9d7e76dd7850
+size 1002
