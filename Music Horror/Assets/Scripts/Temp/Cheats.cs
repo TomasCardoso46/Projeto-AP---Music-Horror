99a7c6ee1db108b606ea5b40407ac34d9d1d8b07
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-01-05 17:46:51 +0000

    Added Cheats

---
+++ Music Horror/Assets/Scripts/Temp/Cheats.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3495b9406aae925b6c704dbeb3a265599bc4b8702c3a573b3db9e22a3d664a9a
+size 1815
--- Music Horror/Assets/Scripts/Temp/DeleteEnemy.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e9c56d09c411391b33879f2dd4b137bb99bc08550ddb1c8e4ccac55fc20c28e0
-size 354
