25da05fadf3b324ffcbbdf6570cb779ae25cfdfb
Hugo Silva <146845704+HugoSilva2@users.noreply.github.com>
2026-03-04 17:01:31 +0000

    Camera reverted back to original

--- Music Horror/Assets/Scripts/Player/Movement/FirstPersonController.cs
+++ Music Horror/Assets/Scripts/Player/Movement/FirstPersonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:468a72dfe8365472187804001b328e0dde24dd8b7244bbb194cc7ab7ee264203
-size 5557
+oid sha256:1be02b5685a0416085ebdbf5793eb59f3c5364d2b9d30f59d38b86a0b62c9ace
+size 5559
