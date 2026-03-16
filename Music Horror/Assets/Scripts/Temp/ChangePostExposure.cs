476f816a7210f57e2a9d7e4767fd1319831c388b
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-03-16 01:44:44 +0000

    fixes to some lighting issues + cheats

--- Music Horror/Assets/Scripts/Temp/ChangePostExposure.cs
+++ Music Horror/Assets/Scripts/Temp/ChangePostExposure.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1af06eda0f29f942960f79c1ce9d4bcad2e43ba74aa9066c3f3edda749ffc18e
-size 940
+oid sha256:355714fc77f4e25cbdc0acadcf9667cd4794b7ac67e9964d6fa191aa6285e396
+size 1186
---
+++ Music Horror/Assets/Scripts/Temp/Gamma.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:43dd3bb385024094fa9d2cc89d187dd4668d1266890fc18fe90dd3a367aa753e
+size 1188
