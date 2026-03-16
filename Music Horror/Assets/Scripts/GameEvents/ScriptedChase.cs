c09c3258dbec75dc2c8491a767f08869e7725f78
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-03-16 16:08:44 +0000

    Locked bait for chase scene

--- Music Horror/Assets/Scripts/GameEvents/ScriptedChase.cs
+++ Music Horror/Assets/Scripts/GameEvents/ScriptedChase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5b4ebd31f6f87344b5aefb8971b5964256f0ac4c57e942099a22c72b191ec9a
-size 6731
+oid sha256:b5202bb79e92be0dd211e767f74191e4782c0b9e97df07eced321a1f236f9bb1
+size 6933
---
+++ Music Horror/Assets/Scripts/Temp/SequenceLockController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5f03471dbde66ffacbed290a7800cffcce7a22411a5be86ba8bec94c300aa9a
+size 750
