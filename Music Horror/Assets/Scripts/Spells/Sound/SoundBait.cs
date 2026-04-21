153a6f506075aaf33173578a9c015ea3a78de921
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-04-21 20:55:27 +0100

    Fixed bug with speakers only working once

--- Music Horror/Assets/Scripts/Spells/Sound/SoundBait.cs
+++ Music Horror/Assets/Scripts/Spells/Sound/SoundBait.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9545791515c970a6ef1365a3ed4cd55475f644f45961c3655c6ad5ae996d3f88
-size 2137
+oid sha256:f569790babf4498b78f4b235996f7df57cb8cbf1425daabe68c712de8bd17e08
+size 2140
