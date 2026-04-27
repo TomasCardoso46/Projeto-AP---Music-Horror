b718c1deeb3480a8cde7bc5cfd625f57623d3336
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-04-27 16:28:46 +0100

    Peek mechanic

--- Music Horror/Assets/Scripts/Enemy/Guitarist/Updated/EnemeyController.cs
+++ Music Horror/Assets/Scripts/Enemy/Guitarist/Updated/EnemeyController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe6bd926a07a18a9e4dc4d7a9ad0ce77ee2852d4dbc567dab0a10a3109a52581
-size 11631
+oid sha256:8f0f59929f00be059e3d7a41c12aaeb7d312b4152943c01ec73e85997108ea07
+size 8408
--- Music Horror/Assets/Scripts/World/Hiding/HideSpot.cs
+++ Music Horror/Assets/Scripts/World/Hiding/HideSpot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bdd2c172432ce47ed8281dda7c34ff6897f3906a9a534515bbfc836876356d4
-size 7039
+oid sha256:4650e135fd2bb352be8f65e1b64e2c949590474e13d634ead254ded5f772593a
+size 6202
