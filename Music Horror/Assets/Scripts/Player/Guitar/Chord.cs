6a8034ec1fae7f1a91bd8df3b1d47873e61d08c2
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-05-04 22:41:37 +0100

    Added more sounds

--- Music Horror/Assets/Scripts/Player/Guitar/Chord.cs
+++ Music Horror/Assets/Scripts/Player/Guitar/Chord.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8de9350da68faf06cc09b90869cea6f7bd22b48cf8321ab75373babe7a947e94
-size 4389
+oid sha256:bc3034925d4bdc2b87e3d04550c31213e3218f7c705c6ffb9b0b58dc0efc68dc
+size 4666
