0be8508683f75c8eb4fddb78a667e49d0db4c865
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-05-04 20:37:10 +0100

    Fixed gamma for main scene

--- Music Horror/Assets/MenuStuff/Settings/SettingsManager.cs
+++ Music Horror/Assets/MenuStuff/Settings/SettingsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:510151243d8ac062a3091c5c565b3df2d4b3c9a4c437c97690825fd32b57e0d7
-size 3119
+oid sha256:23bb8e86338c6effc6a3da24cc2d34d5fd2ac73bac46c25b74b38e1d7f3e4939
+size 3648
