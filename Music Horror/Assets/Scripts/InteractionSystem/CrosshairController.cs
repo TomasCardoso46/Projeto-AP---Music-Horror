7f6b5c33650afc57cc080bc10cdb7441f6553a5d
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-06-03 14:06:43 +0100

    smaller crosshair, grows for interactions

--- Music Horror/Assets/Scripts/InteractionSystem/CrosshairController.cs
+++ Music Horror/Assets/Scripts/InteractionSystem/CrosshairController.cs
@@ -9,8 +9,31 @@ public class CrosshairController : MonoBehaviour
     [SerializeField] private Color defaultColor = Color.white;
     [SerializeField] private Color interactColor = Color.green;
 
+    [Header("Scale")]
+    [SerializeField] private float defaultScale = 1f;
+    [SerializeField] private float interactScale = 1.3f;
+    [SerializeField] private float scaleLerpSpeed = 10f;
+
+    private RectTransform rectTransform;
+    private float targetScale;
+
+    private void Awake()
+    {
+        rectTransform = crosshairImage.GetComponent<RectTransform>();
+        targetScale = defaultScale;
+    }
+
+    private void Update()
+    {
+        float currentScale = rectTransform.localScale.x;
+        float newScale = Mathf.Lerp(currentScale, targetScale, Time.deltaTime * scaleLerpSpeed);
+
+        rectTransform.localScale = new Vector3(newScale, newScale, 1f);
+    }
+
     public void SetInteractState(bool canInteract)
     {
         crosshairImage.color = canInteract ? interactColor : defaultColor;
+        targetScale = canInteract ? interactScale : defaultScale;
     }
 }
