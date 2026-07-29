7474cf69b7c33f06b58e06e05e6b998376dcdb2f
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-07-29 14:15:58 +0100

    Prototype to activate trails depending on chords played

--- Music Horror/Assets/Scripts/Puzzle/Paintings/PaintingChordsDetector.cs
+++ Music Horror/Assets/Scripts/Puzzle/Paintings/PaintingChordsDetector.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PaintingChordsDetector : MonoBehaviour
 {
+    [SerializeField] private GameObject objectToActivate;
+    [SerializeField] private KeyCode Chord;
+    public bool playerInside = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,19 @@ public class PaintingChordsDetector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerInside && Input.GetKeyDown(Chord))
+        {
+            objectToActivate.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) playerInside = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) playerInside = false;
     }
 }
