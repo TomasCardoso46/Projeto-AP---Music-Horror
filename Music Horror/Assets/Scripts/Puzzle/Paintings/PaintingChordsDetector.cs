edf7a871f785cf0ab2206d2223ff7d18ccff20b6
TomasCardoso46 <tomascardoso4633@gmail.com>
2026-07-30 12:15:06 +0100

    Painting queue

--- Music Horror/Assets/Scripts/Puzzle/Paintings/PaintingChordsDetector.cs
+++ Music Horror/Assets/Scripts/Puzzle/Paintings/PaintingChordsDetector.cs
@@ -7,37 +7,56 @@ public class PaintingChordsDetector : MonoBehaviour
     [SerializeField] private KeyCode chord;
     [SerializeField] private int queuePosition;
     [SerializeField] private bool isLast;
-    [SerializeField] private Color color;
-    private bool playerInside = false;
-    private bool correctOrder = false;
-    void Start()
+
+    private bool playerInside;
+
+    private PaintingChordsManager manager;
+
+    public int QueuePosition => queuePosition;
+
+    public void Initialize(PaintingChordsManager manager)
     {
-        
+        this.manager = manager;
     }
 
     void Update()
     {
-        if (playerInside && Input.GetKeyDown(chord))
+        if (!playerInside)
+            return;
+
+        if (Input.GetKeyDown(chord))
         {
-            if (correctOrder)
-            {
-                objectToActivate.SetActive(true);
-                if (isLast)
-                {
-                    objectToDeactivate.SetActive(false);
-                }
-            }
-            
+            manager.TryInteract(this);
         }
+    }
+
+    public void CorrectInteraction()
+    {
+        if (objectToActivate != null)
+            objectToActivate.SetActive(true);
+
+        if (isLast && objectToDeactivate != null)
+            objectToDeactivate.SetActive(false);
+    }
+
+    public void ResetPainting()
+    {
+        if (objectToActivate != null)
+            objectToActivate.SetActive(false);
+
+        if (isLast && objectToDeactivate != null)
+            objectToDeactivate.SetActive(true);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) playerInside = true;
+        if (other.CompareTag("Player"))
+            playerInside = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) playerInside = false;
+        if (other.CompareTag("Player"))
+            playerInside = false;
     }
 }
--- Music Horror/Assets/Scripts/Puzzle/Paintings/PaintingChordsManager.cs
+++ Music Horror/Assets/Scripts/Puzzle/Paintings/PaintingChordsManager.cs
@@ -1,16 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PaintingChordsManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private List<PaintingChordsDetector> paintings = new();
+
+    private int currentQueuePosition = 0;
+
+    private void Awake()
     {
-        
+        foreach (PaintingChordsDetector painting in paintings)
+        {
+            painting.Initialize(this);
+        }
+
+        ResetPuzzle();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool TryInteract(PaintingChordsDetector painting)
     {
-        
+        if (painting.QueuePosition == currentQueuePosition)
+        {
+            painting.CorrectInteraction();
+
+            currentQueuePosition++;
+
+            return true;
+        }
+
+        ResetPuzzle();
+        return false;
+    }
+
+    public void ResetPuzzle()
+    {
+        currentQueuePosition = 0;
+
+        foreach (PaintingChordsDetector painting in paintings)
+        {
+            painting.ResetPainting();
+        }
     }
 }
